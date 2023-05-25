65c5774471d7e42e8468badfe9ac885f7787e339
monandkey <satoru070505@gmail.com>
2023-05-25 15:44:27 +0000

    Modification of options

--- cli.go
+++ cli.go
@@ -7,9 +7,11 @@ import (
 var cmd = &cobra.Command{}
 
 type params struct {
-	namespace           string
-	allNamespace        bool
 	kubeconfig          string
+	namespace           string
+	resource            string
+	allNamespace        bool
+	allResource         bool
 	notDisplayPod       bool
 	notDisplayContainer bool
 }
@@ -21,10 +23,12 @@ func init() {
 	cmd.Use = "k8sr"
 	cmd.Short = "Displays the CPU and Memory Requests and Limits defined in the kubernetes workload resource"
 	cmd.Flags().StringVarP(&params.namespace, "namespace", "n", params.namespace, "Kubernetes namespace to use. Default to namespace configured in Kubernetes context")
-	cmd.Flags().StringVarP(&params.kubeconfig, "kubeconfig", "k", params.kubeconfig, " Path to kubeconfig file to use")
-	cmd.Flags().BoolVarP(&params.allNamespace, "all-namespaces", "A", params.allNamespace, "")
-	cmd.Flags().BoolVarP(&params.notDisplayPod, "not-display-pod", "p", params.notDisplayPod, "")
-	cmd.Flags().BoolVarP(&params.notDisplayContainer, "not-display-container", "c", params.notDisplayContainer, "")
+	cmd.Flags().StringVarP(&params.resource, "resource", "r", params.resource, "Choose from kubernetes workload resources.\nSpecifically, choose from Pod, ReplicaSet, Deployment, StatefulSet, DaemonSet, Job, and CronJob.")
+	cmd.Flags().StringVarP(&params.kubeconfig, "kubeconfig", "k", params.kubeconfig, "Path to kubeconfig file to use.")
+	cmd.Flags().BoolVarP(&params.allNamespace, "all-namespaces", "A", params.allNamespace, "Enable only if you want to cover all namespaces.")
+	cmd.Flags().BoolVarP(&params.allResource, "all-resource", "R", params.allResource, "Enable only if you want to see all workload resources.")
+	cmd.Flags().BoolVarP(&params.notDisplayPod, "not-display-pod", "p", params.notDisplayPod, "Enable only if you want to display only the Container without displaying the Pod.\nThis option is valid only if you want to display Pod resources.")
+	cmd.Flags().BoolVarP(&params.notDisplayContainer, "not-display-container", "c", params.notDisplayContainer, "Enable the flag if you do not want the container to be displayed.\nThis option is valid only if you want to display Pod resources.")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		return exec(params)
