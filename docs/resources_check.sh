80d19c173aebe0adcb6742ab8a6173e08f11a1e4
monandkey <satoru070505@gmail.com>
2023-05-27 11:11:17 +0000

    Script Modification

--- docs/resources_check.sh
+++ docs/resources_check.sh
@@ -8,15 +8,26 @@ k8sResource=(
   "deployment",
   "statefulset",
   "daemonset",
-  "cronjob"
+  "cronjob",
+  "pod"
 )
 
 # Define offsets to containers and InitContainers.
 k8sOffset=(
-  ".spec.template.spec",                 # deployment
-  ".spec.template.spec",                 # statefulset
-  ".spec.template.spec",                 # daemonset
-  ".spec.jobTemplate.spec.template.spec" # cronjob
+  "spec.template.spec",                  # deployment
+  "spec.template.spec",                  # statefulset
+  "spec.template.spec",                  # daemonset
+  "spec.jobTemplate.spec.template.spec", # cronjob
+  "spec"                                 # pod
+)
+
+# If a flag of 1 is entered, the output of that resource is skipped.
+k8sResourceSkipFlag=(
+  0, # deployment
+  0, # statefulset
+  0, # daemonset
+  0, # cronjob
+  1, # pod
 )
 
 # Split up the command because it is too long to describe it normally in the command.
@@ -47,7 +58,7 @@ function cleanUpResultfile () {
 # Output headers for CSV. HEADER_FLAG is used to determine whether to output.
 function headerOutput () {
   if [ $HEADER_FLAG -eq 1 ]; then
-    echo NAMESPACES,POD,CONTAINER,REQUEST_CPU,REQUEST_MEM,LIMIT_CPU,LIMIT_MEM | tee $RESULT_FILE
+    echo NAMESPACES,RESOURCES,METADATA_NAME,CONTAINER,REQUEST_CPU,REQUEST_MEM,LIMIT_CPU,LIMIT_MEM | tee $RESULT_FILE
     HEADER_FLAG=0
   fi
 }
@@ -56,15 +67,16 @@ function headerOutput () {
 # Logs to console and file.
 function resultOutput () {
   ns=$1
-  targetName=$2
-  container=$3
-  reqCPUList=$4
-  reqMemList=$5
-  limCPUList=$6
-  limMemList=$7
+  resources=$2
+  metadataName=$3
+  container=$4
+  reqCPUList=$5
+  reqMemList=$6
+  limCPUList=$7
+  limMemList=$8
 
   for cnum in `seq 0 $((${#container[*]}-1))`; do
-    echo $ns,$targetName,${conList[$cnum]},${reqCPUList[$cnum]},${reqMemList[$cnum]},${limCPUList[$cnum]},${limMemList[$cnum]} | tee -a $RESULT_FILE
+    echo $ns,$resources,$metadataName,${conList[$cnum]},${reqCPUList[$cnum]},${reqMemList[$cnum]},${limCPUList[$cnum]},${limMemList[$cnum]} | tee -a $RESULT_FILE
   done
 }
 
@@ -73,26 +85,26 @@ function resultOutput () {
 function retrievePerContainerInfo () {
   ns=$1
   rnum=$2
-  targetName=$3
+  metadataName=$3
 
   # Retrieve the container name of the container and initContainer.
-  conList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.containers[*].name}")
-  initConList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.initContainers[*].name}")
+  conList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.containers[*].name}")
+  initConList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.initContainers[*].name}")
 
   if [ $((${#conList[0]}-1)) -ne 0 ]; then
-    conReqCPUList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.contaienrs[*].$k8sReqCPU}")
-    conReqMemList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.contaienrs[*].$k8sReqMem}")
-    conLimCPUList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.contaienrs[*].$k8sLimCPU}")
-    conLimMemList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.contaienrs[*].$k8sLimMem}")
-    resultOutput $ns $targetName $conList $conReqCPUList $conReqMemList $conLimCPUList $conLimMemList
+    conReqCPUList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.contaienrs[*].$k8sReqCPU}")
+    conReqMemList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.contaienrs[*].$k8sReqMem}")
+    conLimCPUList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.contaienrs[*].$k8sLimCPU}")
+    conLimMemList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.contaienrs[*].$k8sLimMem}")
+    resultOutput $ns ${k8sResource[$rnum]} $metadataName $conList $conReqCPUList $conReqMemList $conLimCPUList $conLimMemList
   fi
 
   if [ $((${#initConList[0]}-1)) -ne 0 ]; then
-    iconReqCPUList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.initContainers[*].$k8sReqCPU}")
-    iconReqMemList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.initContainers[*].$k8sReqMem}")
-    iconLimCPUList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.initContainers[*].$k8sLimCPU}")
-    iconLimMemList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $targetName -o jsonpath="{${k8sOffset[$rnum]}.initContainers[*].$k8sLimMem}")
-    resultOutput $ns $targetName $iconList $iconReqCPUList $iconReqMemList $iconLimCPUList $iconLimMemList
+    iconReqCPUList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.initContainers[*].$k8sReqCPU}")
+    iconReqMemList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.initContainers[*].$k8sReqMem}")
+    iconLimCPUList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.initContainers[*].$k8sLimCPU}")
+    iconLimMemList=$($k8sCmd get ${k8sResource[$rnum]} -n $ns $metadataName -o jsonpath="{.${k8sOffset[$rnum]}.initContainers[*].$k8sLimMem}")
+    resultOutput $ns ${k8sResource[$rnum]} $metadataName $iconList $iconReqCPUList $iconReqMemList $iconLimCPUList $iconLimMemList
   fi
 }
 
@@ -102,8 +114,10 @@ function retrievePerResource () {
   ns=$1
 
   for rnum in `seq 0 $((${#k8sResource[*]}-1))`; do
-    for targetName in $($k8sCmd get ${k8sResource[$rnum]} -n $ns 2> /dev/null | grep -v NAME | awk '{print $1}'); do
-      retrievePerContainerInfo $ns $rnum $targetName
+    if [ ${k8sResourceSkipFlag[$rnum]} -eq 1 ]; then continue; fi
+
+    for metadataName in $($k8sCmd get ${k8sResource[$rnum]} -n $ns 2> /dev/null | grep -v NAME | awk '{print $1}'); do
+      retrievePerContainerInfo $ns $rnum $metadataName
     done
   done
 }
