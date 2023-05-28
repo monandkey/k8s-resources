11802aab38337e0bbe3d228eebb3ed9c2bd5370e
monandkey <satoru070505@gmail.com>
2023-05-28 07:42:52 +0000

    Create functions to output information

--- display.go
+++ display.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	appsv1 "k8s.io/api/apps/v1"
-	batchv1 "k8s.io/api/batch/v1"
+	"fmt"
+	"strings"
+
 	corev1 "k8s.io/api/core/v1"
 )
 
 const (
-	HEADER_POD_NAME       = "POD NAME"
-	HEADER_CONTAINER_NAME = "CONTAINER NAME"
-	HEADER_REQUEST_CPU    = "REQUEST CPU"
-	HEADER_REQUEST_MEMORY = "REQUEST MEMORY"
-	HEADER_LIMIT_CPU      = "LIMIT CPU"
-	HEADER_LIMIT_MEMORY   = "LIMIT MEMORY"
+	headerPodName       = "POD NAME"
+	headerContainerName = "CONTAINER NAME"
+	headerRequestCPU    = "REQUEST CPU"
+	headerRequestMemory = "REQUEST MEMORY"
+	headerLimitCPU      = "LIMIT CPU"
+	headerLimitMemory   = "LIMIT MEMORY"
 )
-
-func compareLengths(left, right int) int {
-	if left < right {
-		return right
-	}
-	return left
-}
 
 func paddingSet(name string, length int, max int) int {
 	if max > len(name) {
@@ -31,87 +25,44 @@ func paddingSet(name string, length int, max int) int {
 	return max + 1
 }
 
-func displayPodList(podList *corev1.PodList) error {
-	padding := map[string]int{
-		"podName":       0,
-		"containerName": 0,
-		"requestCPU":    0,
-		"requestMemory": 0,
-		"LimitCPU":      0,
-		"LimitMemory":   0,
-	}
-	totalResourceList := map[string]map[string]int{}
-	for _, v := range podList.Items {
-		var (
-			totalReqCPU = 0
-			totalReqMem = 0
-			totalLimCPU = 0
-			totalLimMem = 0
-		)
-		for i := range v.Spec.Containers {
-			container := v.Spec.Containers[i]
-			padding["podName"] = compareLengths(padding["podName"], len(v.GetName()))
-			padding["containerName"] = compareLengths(padding["containerName"], len(container.Name))
-			padding["requestCPU"] = compareLengths(padding["requestCPU"], container.Resources.Requests.Cpu().Size())
-			padding["requestMemory"] = compareLengths(padding["requestMemory"], container.Resources.Requests.Memory().Size())
-			padding["LimitCPU"] = compareLengths(padding["LimitCPU"], container.Resources.Limits.Cpu().Size())
-			padding["LimitMemory"] = compareLengths(padding["LimitMemory"], container.Resources.Limits.Memory().Size())
-			totalReqCPU += container.Resources.Requests.Cpu().Size()
-			totalReqMem += container.Resources.Requests.Memory().Size()
-			totalLimCPU += container.Resources.Limits.Cpu().Size()
-			totalLimMem += container.Resources.Limits.Memory().Size()
-		}
-		for i := range v.Spec.InitContainers {
-			container := v.Spec.InitContainers[i]
-			padding["podName"] = compareLengths(padding["podName"], len(v.GetName()))
-			padding["containerName"] = compareLengths(padding["containerName"], len(container.Name))
-			padding["requestCPU"] = compareLengths(padding["requestCPU"], container.Resources.Requests.Cpu().Size())
-			padding["requestMemory"] = compareLengths(padding["requestMemory"], container.Resources.Requests.Memory().Size())
-			padding["LimitCPU"] = compareLengths(padding["LimitCPU"], container.Resources.Limits.Cpu().Size())
-			padding["LimitMemory"] = compareLengths(padding["LimitMemory"], container.Resources.Limits.Memory().Size())
-			totalReqCPU += container.Resources.Requests.Cpu().Size()
-			totalReqMem += container.Resources.Requests.Memory().Size()
-			totalLimCPU += container.Resources.Limits.Cpu().Size()
-			totalLimMem += container.Resources.Limits.Memory().Size()
-		}
-		totalResourceList[v.GetName()]["requestCPU"] = totalReqCPU
-		totalResourceList[v.GetName()]["requestMemory"] = totalReqMem
-		totalResourceList[v.GetName()]["LimitCPU"] = totalLimCPU
-		totalResourceList[v.GetName()]["LimitMemory"] = totalLimMem
-	}
-
-	for _, pod := range podList.Items {
-		podNamePadding := paddingSet(HEADER_POD_NAME, len(pod.GetName()), padding["podName"])
-		containerNamePadding := paddingSet(HEADER_CONTAINER_NAME, len(""), padding["containerName"])
-		requestCPUPadding := paddingSet(HEADER_REQUEST_CPU, totalResourceList[pod.Name]["requestCPU"], padding["requestCPU"])
-		requestMemoryPadding := paddingSet(HEADER_REQUEST_MEMORY, totalResourceList[pod.Name]["requestMemory"], padding["requestMemory"])
-		LimitCPUPadding := paddingSet(HEADER_LIMIT_CPU, totalResourceList[pod.Name]["LimitCPU"], padding["LimitCPU"])
-		LimitMemoryPadding := paddingSet(HEADER_LIMIT_MEMORY, totalResourceList[pod.Name]["LimitMemory"], padding["LimitMemory"])
-	}
-
-	return nil
+func outputHeader(padding padding) {
+	podNamePadding := paddingSet(headerPodName, len(headerPodName), padding.podName)
+	containerNamePadding := paddingSet(headerContainerName, len(headerContainerName), padding.containerName)
+	requestCPUPadding := paddingSet(headerRequestCPU, len(headerRequestCPU), padding.requestCPU)
+	requestMemoryPadding := paddingSet(headerRequestMemory, len(headerRequestMemory), padding.requestMemory)
+	limitCPUPadding := paddingSet(headerLimitCPU, len(headerLimitCPU), padding.LimitCPU)
+	limitMemoryPadding := paddingSet(headerLimitMemory, len(headerLimitMemory), padding.LimitMemory)
+	fmt.Printf("| %s%s ", headerPodName, strings.Repeat(" ", podNamePadding))
+	fmt.Printf("| %s%s ", headerContainerName, strings.Repeat(" ", containerNamePadding))
+	fmt.Printf("| %s%s ", headerRequestCPU, strings.Repeat(" ", requestCPUPadding))
+	fmt.Printf("| %s%s ", headerRequestMemory, strings.Repeat(" ", requestMemoryPadding))
+	fmt.Printf("| %s%s ", headerLimitCPU, strings.Repeat(" ", limitCPUPadding))
+	fmt.Printf("| %s%s ", headerLimitMemory, strings.Repeat(" ", limitMemoryPadding))
+	fmt.Printf("|\n")
 }
 
-func displayReplicaSetList(*appsv1.ReplicaSetList) error {
-	return nil
+func outputFrame(padding padding) {
+	fmt.Printf("+%s", strings.Repeat("-", padding.podName))
+	fmt.Printf("+%s", strings.Repeat("-", padding.containerName))
+	fmt.Printf("+%s", strings.Repeat("-", padding.requestCPU))
+	fmt.Printf("+%s", strings.Repeat("-", padding.requestMemory))
+	fmt.Printf("+%s", strings.Repeat("-", padding.LimitCPU))
+	fmt.Printf("+%s", strings.Repeat("-", padding.LimitMemory))
+	fmt.Printf("+\n")
 }
 
-func displayDeploymentList(*appsv1.DeploymentList) error {
-	return nil
-}
-
-func displayStatefulSetList(*appsv1.StatefulSetList) error {
-	return nil
-}
-
-func displayDaemonSetList(*appsv1.DaemonSetList) error {
-	return nil
-}
-
-func displayJobList(*batchv1.JobList) error {
-	return nil
-}
-
-func displayCronJobList(*batchv1.CronJobList) error {
-	return nil
+func outputBody(padding padding, container corev1.Container, podName string, resourceList map[string]containerResources) {
+	podNamePadding := paddingSet(headerPodName, len(podName), padding.podName)
+	requestCPUPadding := paddingSet(headerRequestCPU, resourceList[podName].requestCPU, padding.requestCPU)
+	requestMemoryPadding := paddingSet(headerRequestMemory, resourceList[podName].requestMemory, padding.requestMemory)
+	limitCPUPadding := paddingSet(headerLimitCPU, resourceList[podName].limitCPU, padding.LimitCPU)
+	limitMemoryPadding := paddingSet(headerLimitMemory, resourceList[podName].limitMemory, padding.LimitMemory)
+	containerNamePadding := paddingSet(headerContainerName, len(container.Name), padding.containerName)
+	fmt.Printf("| %s%s ", podName, strings.Repeat(" ", podNamePadding))
+	fmt.Printf("| %s%s ", container.Name, strings.Repeat(" ", containerNamePadding))
+	fmt.Printf("| %d%s ", container.Resources.Requests.Cpu().Size(), strings.Repeat(" ", requestCPUPadding))
+	fmt.Printf("| %d%s ", container.Resources.Requests.Memory().Size(), strings.Repeat(" ", requestMemoryPadding))
+	fmt.Printf("| %d%s ", container.Resources.Limits.Cpu().Size(), strings.Repeat(" ", limitCPUPadding))
+	fmt.Printf("| %d%s ", container.Resources.Limits.Memory().Size(), strings.Repeat(" ", limitMemoryPadding))
+	fmt.Printf("|\n")
 }
