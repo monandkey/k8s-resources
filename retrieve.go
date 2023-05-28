b43e7ce81954f953c3258cdbbf8815bdefdf0f11
monandkey <satoru070505@gmail.com>
2023-05-28 09:47:28 +0000

    Correction of notation

--- retrieve.go
+++ retrieve.go
@@ -1,14 +1,16 @@
 package main
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	corev1 "k8s.io/api/core/v1"
+)
 
-type padding struct {
+type maxLength struct {
 	podName       int
 	containerName int
 	requestCPU    int
 	requestMemory int
-	LimitCPU      int
-	LimitMemory   int
+	limitCPU      int
+	limitMemory   int
 }
 
 func compareLengths(left, right int) int {
@@ -18,23 +20,34 @@ func compareLengths(left, right int) int {
 	return left
 }
 
-func retrievePaddingLength(lp padding, podName string, container corev1.Container) padding {
-	return padding{
-		podName:       compareLengths(lp.podName, len(podName)),
-		containerName: compareLengths(lp.containerName, len(container.Name)),
-		requestCPU:    compareLengths(lp.requestCPU, container.Resources.Requests.Cpu().Size()),
-		requestMemory: compareLengths(lp.requestMemory, container.Resources.Requests.Memory().Size()),
-		LimitCPU:      compareLengths(lp.LimitCPU, container.Resources.Limits.Cpu().Size()),
-		LimitMemory:   compareLengths(lp.LimitMemory, container.Resources.Limits.Memory().Size()),
+func retrieveHeaderPaddingLength(lp maxLength) maxLength {
+	return maxLength{
+		podName:       compareLengths(lp.podName, len(headerPodName)),
+		containerName: compareLengths(lp.containerName, len(headerContainerName)),
+		requestCPU:    compareLengths(lp.requestCPU, len(headerRequestCPU)),
+		requestMemory: compareLengths(lp.requestMemory, len(headerRequestMemory)),
+		limitCPU:      compareLengths(lp.limitCPU, len(headerLimitCPU)),
+		limitMemory:   compareLengths(lp.limitMemory, len(headerLimitMemory)),
 	}
 }
 
-func retrievePadding(lp padding, podName string, containers, initContainers []corev1.Container) padding {
+func retrieveMaxLength(lp maxLength, podName string, container corev1.Container) maxLength {
+	return maxLength{
+		podName:       compareLengths(lp.podName, len(podName)),
+		containerName: compareLengths(lp.containerName, len(container.Name)),
+		requestCPU:    compareLengths(lp.requestCPU, len(container.Resources.Requests.Cpu().String())),
+		requestMemory: compareLengths(lp.requestMemory, len(container.Resources.Requests.Memory().String())),
+		limitCPU:      compareLengths(lp.limitCPU, len(container.Resources.Limits.Cpu().String())),
+		limitMemory:   compareLengths(lp.limitMemory, len(container.Resources.Limits.Memory().String())),
+	}
+}
+
+func retrieveMaxLengths(lp maxLength, podName string, containers, initContainers []corev1.Container) maxLength {
 	for _, container := range containers {
-		lp = retrievePaddingLength(lp, podName, container)
+		lp = retrieveMaxLength(lp, podName, container)
 	}
 	for _, container := range initContainers {
-		lp = retrievePaddingLength(lp, podName, container)
+		lp = retrieveMaxLength(lp, podName, container)
 	}
 	return lp
 }
